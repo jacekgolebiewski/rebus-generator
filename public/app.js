3754acbf4a0f1116e46879bc1c62384d0d662168
jgolebiewski <jacekg1993@gmail.com>
2020-08-10 16:45:18 +0200

    uzupełnienie słownika i implementacja reprezentacji tekstowej rebusu

--- public/app.js
+++ public/app.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
 const generator = require('./src/generator.js')
 
-async function generate() {
+async function generateText() {
     const text = document.getElementById("text").value;
     const dictionary = readJson('public/dictionary.json').dictionary;
     const result = generator.generate(text, dictionary);
-    await drawRebus(result);
+    document.getElementById("rebus-text").value = result;
+}
+
+async function generateRebus() {
+    await drawRebus(document.getElementById("rebus-text").value);
 }
 
 function exportRebus() {
@@ -17,7 +21,7 @@ function exportRebus() {
 }
 
 function getExportFileName() {
-    return `${new Date().getYear()+1900}-${new Date().getMonth()+1 >= 10 ? new Date().getMonth()+1 : '0'+(new Date().getMonth()+1)}-${new Date().getDate() >= 10 ? new Date().getDate() : '0'+(new Date().getDate())}-generated.jpg`;
+    return `${new Date().getYear() + 1900}-${new Date().getMonth() + 1 >= 10 ? new Date().getMonth() + 1 : '0' + (new Date().getMonth() + 1)}-${new Date().getDate() >= 10 ? new Date().getDate() : '0' + (new Date().getDate())}-generated.jpg`;
 }
 
 function readJson(fileName) {
@@ -34,6 +38,8 @@ const ASSETS_FOLDER = 'assets/';
 let CANVAS;
 
 function drawRebus(result) {
+    const decodedRebus = decodeRebusText(result);
+
     const fileNames = fs.readdirSync(FOLDER_FS_PREFIX + ASSETS_FOLDER);
     let sketch = function (p) {
         p.setup = async () => {
@@ -44,52 +50,68 @@ function drawRebus(result) {
             p.background(255);
             p.textSize(32);
             let offset = 0;
-            for (const wordElements of result) {
+            for (const word of decodedRebus) {
                 offset += DISTANCE;
-                for (const element of wordElements) {
-                    if (element.type === 'LITERAL') {
+                for (const element of word) {
+                    let firstExpression = element[0];
+                    if (firstExpression[0] === generator.LITERAL_PREFIX) {
                         p.fill('red');
                         p.stroke('red');
                         p.strokeWeight(1);
-                        p.text(element.text, offset, HEIGHT / 2 - 20);
+                        p.text(firstExpression.substr(1), offset, HEIGHT / 2 - 20);
                     } else {
-                        await drawImage(p, element, offset, fileNames);
+                        await drawImage(p, firstExpression.substr(1), offset, fileNames);
                         let offset2 = offset;
-                        element.diff.forEach(diffRecord => {
-                            if (diffRecord.removed) {
+                        for(const elementExp of element) {
+                            const expOperator = elementExp[0];
+                            const expText = elementExp.substr(1);
+                            const expCount = expText.length;
+                            if (expOperator === generator.REMOVE_PREFIX) {
                                 p.fill('black');
                                 p.stroke('black');
                                 p.strokeWeight(1);
-                                p.text(diffRecord.value, offset2, DISTANCE * 6 + ELEMENT_SIZE);
+                                p.text(expText, offset2, DISTANCE * 6 + ELEMENT_SIZE);
                                 p.fill('red');
                                 p.stroke('red');
                                 p.strokeWeight(2);
                                 p.line(offset2, DISTANCE * 6 + ELEMENT_SIZE,
-                                    offset2 + diffRecord.count * 12 + DISTANCE, DISTANCE * 4 + ELEMENT_SIZE);
-                            } else if (diffRecord.added) {
+                                    offset2 + expCount * 12 + DISTANCE, DISTANCE * 4 + ELEMENT_SIZE);
+                            } else if (expOperator === generator.ADD_PREFIX) {
                                 p.fill('green');
                                 p.stroke('green');
                                 p.strokeWeight(1);
-                                p.text(diffRecord.value, offset2, DISTANCE * 6 + ELEMENT_SIZE);
+                                p.text(expText, offset2, DISTANCE * 6 + ELEMENT_SIZE);
                             }
-                            offset2 += diffRecord.count * 14 + DISTANCE;
-                        })
+                            if (expOperator !== generator.ELEMENT_PREFIX) {
+                                offset2 += expCount * 14 + DISTANCE;
+                            }
+                        }
                     }
                     offset += calculateElementWidth(element);
                 }
                 offset += DISTANCE * 6;
             }
         }
-    };
+    }
     new p5(sketch, 'canvas-wrapper');
 }
 
-async function drawImage(p, element, offset, fileNames) {
-    const fileName = getFileName(element.name, fileNames);
+function decodeRebusText(result) {
+    return result.split(generator.WORD_SEPARATOR)
+        .map(elementsString => elementsString.split(']').join('')
+            .split('[')
+            .filter(el => !!el.length)
+            .map(element => element.split(/([$!+*\-][AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuWwYyZzŹźŻż\w]+)/)
+                .filter(el => !!el.length)));
+
+}
+
+async function drawImage(p, elementName, offset, fileNames) {
+    const fileName = getFileName(elementName, fileNames);
     if (!fileName) {
         return new Promise(resolve => {
             p.fill('gray');
-            p.text(`[${element.name}]`, offset, HEIGHT / 2 - 20);
+            p.text(`[${elementName}]`, offset, HEIGHT / 2 - 20);
             resolve(true)
         });
     }
--- public/src/generator.js
+++ public/src/generator.js
@@ -1,47 +1,61 @@
 const diff = require('diff');
 
+
 const generator = {};
+
+generator.WORD_SEPARATOR = ',,';
+generator.ELEMENT_PREFIX = '$';
+generator.LITERAL_PREFIX = '!';
+generator.ADD_PREFIX = '+';
+generator.REMOVE_PREFIX = '-';
+generator.SKIP_PREFIX = '*';
+generator.operations = [
+
+]
 
 generator.generate = (text, dictionary) => {
     const words = text.split(' ');
-    return words.map(word => transform(word, dictionary))
+    return words.map(word => transform(word, dictionary)).join(generator.WORD_SEPARATOR)
 }
 
 function transform(word, dictionary) {
     if (word.length < 2) {
-        return [buildLiteral(word)];
+        return buildLiteral(word);
     }
-    let result = []
+    let result = '';
     let correctWords = dictionary.filter(dictWord => fits(word, dictWord))
     if (correctWords.length === 0) {
-        return [buildLiteral(word)];
+        return buildLiteral(word);
     }
     let chosenWord = randomFromArray(correctWords);
     let mainPart = getMainPart(word, chosenWord);
     let wordLeftParts = word.split(mainPart);
     if (wordLeftParts[0].length > 0) {
-        result = result.concat(transform(wordLeftParts[0], dictionary));
+        result += transform(wordLeftParts[0], dictionary);
     }
-    result.push(buildElement(chosenWord, mainPart));
+    result += buildElement(chosenWord, mainPart);
     if (wordLeftParts[1].length > 0) {
-        result = result.concat(transform(wordLeftParts[1], dictionary));
+        result += transform(wordLeftParts[1], dictionary);
     }
     return result;
 }
 
 function buildLiteral(word) {
-    return {
-        type: 'LITERAL',
-        text: word
-    }
+    return `[${generator.LITERAL_PREFIX}${word}]`;
 }
 
 function buildElement(chosenWord, mainPart) {
-    return {
-        type: 'ELEMENT',
-        name: chosenWord,
-        diff: diff.diffChars(chosenWord, mainPart)
-    };
+    let result = `[${generator.ELEMENT_PREFIX}${chosenWord}`;
+    diff.diffChars(chosenWord, mainPart).forEach(diffRec => {
+        if(diffRec.removed) {
+            result += `${generator.REMOVE_PREFIX}${diffRec.value}`;
+        } else if(diffRec.added) {
+            result += `${generator.ADD_PREFIX}${diffRec.value}`;
+        } else {
+            result += `${generator.SKIP_PREFIX}${diffRec.value}`;
+        }
+    });
+    return result + ']';
 }
 
 function fits(text, dictWord) {
